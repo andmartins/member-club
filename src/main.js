df089110614818193634f76a43dd2457ad1c56ff
Anderson Martins <anderson.com@gmail.com>
2025-05-04 17:04:04 -0400

    add css modules, main, assets, styles and index

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+import "./styles/global.css"
+import "./styles/utils.css"
+import "./styles/card.css"
+import "./styles/data.css"
+import "./styles/history.css"
+import "./styles/input.css"
+import "./styles/logo.css"
+import "./styles/progress.css"
