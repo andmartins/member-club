ea4cb3b7951f9469438472f71d9dd5a581f494ff
Anderson Martins <anderson.com@gmail.com>
2025-05-04 17:08:51 -0400

    add modules input.js

---
+++ src/modules/input.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("id-input")
+
+input.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, '')
+    if (value.length > 15) {
+        value = value.slice(0, 15)
+    }
+    
+    let formattedValue = value.replace(/(\d{3})(?=\d)/g, '$1-')
+    e.target.value = formattedValue
+})
